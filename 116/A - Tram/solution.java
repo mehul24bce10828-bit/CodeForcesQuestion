c160b445d60dc98bb4e04924b9f514d55b007fb8
mehul24bce10828-bit <mehul.24bce10828@vitbhopal.ac.in>
2026-08-06 15:03:42 +0530

    Add Tram [A] from Codeforces

---
+++ 116/A - Tram/solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class ATram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int currentPassengers = 0;
+        int maxCapacity = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int exit = sc.nextInt();
+            int enter = sc.nextInt();
+            
+            currentPassengers -= exit;
+            currentPassengers += enter;
+            
+            if (currentPassengers > maxCapacity) {
+                maxCapacity = currentPassengers;
+            }
+        }
+        
+        System.out.println(maxCapacity);
+    }
+}
