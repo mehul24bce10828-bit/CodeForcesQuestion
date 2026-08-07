17c4231b204015f48ae1ac238a27efd73973c60a
mehul24bce10828-bit <mehul.24bce10828@vitbhopal.ac.in>
2026-08-07 18:30:21 +0530

    Add Candies and Two Sisters [A] from Codeforces

---
+++ 1335/A - Candies and Two Sisters/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class ACandiesAndTwoSisters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int t = scanner.nextInt();
+        
+        while (t-- > 0) {
+            long n = scanner.nextLong();
+            
+            long ans = (n - 1) / 2;
+            
+            System.out.println(ans);
+        }
+        
+    }
+}
