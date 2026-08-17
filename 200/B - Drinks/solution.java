1642f8d88a819bfd454c9dbd061f97669dd881e9
mehul24bce10828-bit <mehul.24bce10828@vitbhopal.ac.in>
2026-08-17 11:50:44 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class BDrinks{
+public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         
+         int n=sc.nextInt();
+        
+         double sum=0.0;
+         
+         for(int i=0;i<n;i++){
+            sum +=sc.nextInt();
+         }
+         double result=sum/n;
+         System.out.printf("%.12f
+",result);
+}
+}
